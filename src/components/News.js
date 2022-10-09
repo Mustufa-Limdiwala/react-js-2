cd0b08ac75bc2554f3075e1e169b9a886d41cc33
mustufa-limdiwala <mustufalimdi36@gmail.com>
2022-10-09 07:47:29 +0530

    card style for news item

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,19 @@ import Newsitem from './Newsitem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                this is news components
-                <Newsitem />
+            <div className='container my-3'>
+                <h2>News MOnkey Top headlines</h2>
+                <div className='row'>
+                    <div className="col-md-4">
+                        <Newsitem title="My Title" description="mydesc" />
+                    </div>
+                    <div className="col-md-4">
+                        <Newsitem title="My Title" description="mydesc" />
+                    </div>
+                    <div className="col-md-4">
+                        <Newsitem title="My Title" description="mydesc" />
+                    </div>
+                </div>
             </div>
         )
     }
