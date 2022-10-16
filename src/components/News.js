d81453e96bb30eede3f656532297ccce96c0da13
mustufa-limdiwala <mustufalimdi36@gmail.com>
2022-10-16 15:21:06 +0530

    news app with class

--- src/components/News.js
+++ src/components/News.js
@@ -1,57 +1,112 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
+import React, { Component } from "react";
+import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export class News extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
-    constructor() {
-        super();
+    static defaulProps = {
+        country: "in",
+        pageSize: 6,
+        category: "general",
+    };
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+    };
+    capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+    constructor(props) {
+        super(props);
         this.state = {
-            articles: this.articles,
-            loading: false
-        }
+            articles: [],
+            loading: true,
+            page: 1,
+            totalResults: 0,
+        };
+        document.title = `${this.capitalizeFirstLetter(
+            this.props.category
+        )} - Monkey`;
     }
+    async updateNews() {
+        this.props.setProgress(10);
+
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let data = await fetch(url);
+        let parseData = await data.json();
+        this.setState({
+            articles: parseData.articles,
+            totalResults: parseData.totalResults,
+            loading: false,
+        });
+        this.props.setProgress(100);
+    }
+    async componentDidMount() {
+        this.updateNews();
+    }
+
+    handlePrevClick = async () => {
+        this.setState({ page: this.state.page - 1 });
+        this.updateNews();
+    };
+    handleNextClick = async () => {
+        this.setState({ page: this.state.page + 1 });
+        this.updateNews();
+    };
+
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 });
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=29ed754924714bd4b7b3914eec76f71a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parseData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parseData.articles),
+            totalResults: parseData.totalResults,
+            loading: false,
+        });
+    };
+
     render() {
         return (
-            <div className='container my-3'>
-                <h2>News MOnkey Top headlines</h2>
-                <div className='row'>
-                    {this.state.articles.map((element) => {
-
-                        return <div key={element.url} className="col-md-4">
-                            <Newsitem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} imageurl={element.urlToImage} newsurl={element.url} />
+            <div className="container my-3">
+                <h2 className="text-center">
+                    News Monkey - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
+                    headlines
+                </h2>
+                {this.state.loading && <Spinner />}
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className="container">
+                        <div className="row">
+                            {this.state.articles.map((element) => {
+                                return (
+                                    <div key={element.url} className="col-md-4">
+                                        <Newsitem
+                                            title={element.title ? element.title.slice(0, 45) : ""}
+                                            description={
+                                                element.description
+                                                    ? element.description.slice(0, 88)
+                                                    : ""
+                                            }
+                                            imageurl={element.urlToImage}
+                                            newsurl={element.url}
+                                        />
+                                    </div>
+                                );
+                            })}
                         </div>
-                    })}
-                </div>
+                    </div>
+                </InfiniteScroll>
             </div>
-        )
+        );
     }
 }
 
-export default News
+export default News;
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -5,12 +5,12 @@ export class Newsitem extends Component {
         let { title, description, imageurl, newsurl } = this.props;
         return (
             <div className='my-3'>
-                <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageurl} className="card-img-top" alt="..." />
+                <div className="card">
+                    <img src={imageurl} height="190" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                        <a href={newsurl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
